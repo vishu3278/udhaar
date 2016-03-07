9f3353714f59945cd5ffc92ba5750263a74e55ff
vishu3278 <vishu3278@gmail.com>
2016-03-07 12:43:00 +0530

    app

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-    function showCharm(id){
+    /*function showCharm(id){
         var  charm = $(id).data("charm");
         if (charm.element.data("opened") === true) {
             charm.close();
         } else {
             charm.open();
         }
-    };
+    };*/
 
     function bodyLoad() {
     	document.addEventListener("deviceready",onDeviceReady,false);
