a4e92752c8169253608857fe4644986173748c9b
vishu3278 <vishu3278@gmail.com>
2016-03-07 18:08:19 +0530

    ls

--- app.js
+++ app.js
@@ -18,12 +18,29 @@
         $.Notify({
             caption: 'Device State',
             content: 'Device is ready',
-            type:'info'
+            type:'success'
         });
         
         if(typeof(Storage) !== "undefined") {
             // Code for localStorage/sessionStorage.
+            $.Notify({
+                caption: 'Localstorage',
+                content: 'Localstorage supported.',
+                type:'success'
+            });
+            $("#loginForm").on('submit',function(){
+                localStorage.name = $("input[name=name]").val();
+                localStorage.email = $("input[name=email]").val();
+                localStorage.pass = $("input[name=pass]").val();
+                
+                alert("success");
+            })
         } else {
             // Sorry! No Web Storage support..
+            $.Notify({
+                caption: 'LocalStorage',
+                content: 'LocalStorage is not available',
+                type:'error'
+            });
         }
     }
