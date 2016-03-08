cca588b94d8c3c980855cd64b2abec08c70e4084
vishu3278 <vishu3278@gmail.com>
2016-03-08 17:08:44 +0530

    stor

--- app.js
+++ app.js
@@ -10,15 +10,15 @@
     function bodyLoad() {
         if(typeof(Storage) !== "undefined") {
 //            alert("Supported");
-            $.Notify({
+            /*$.Notify({
                 caption: 'Local Storage',
                 content: 'Local storage is supported in your browser. You can perform localStorage actions.',
                 type: 'success',
                 icon:'<img src="icon2.png">'
-            });
+            });*/
             
         }
-//        document.addEventListener("deviceready", onDeviceReady, false);
+        document.addEventListener("deviceready", onDeviceReady, false);
 //        $("#accordion").accordion();
     }
 
@@ -33,10 +33,17 @@
     };
 
 var store = {};
-$("#loginForm").on('submit',function(){
-    localStorage.name = $("input[name=name]").val();
-    localStorage.email = $("input[name=email]").val();
-    localStorage.pass = $("input[name=pass]").val();
+$("#regButton").on('click',function(){
+    var pass1 = $("input[name=pass1]").val();
+    var pass2 = $("input[name=pass2]").val();
+    
+    if(pass1 === pass2){
+        localStorage.name = $("input[name=name1]").val();
+        localStorage.email = $("input[name=email1]").val();
+        localStorage.pass = $("input[name=pass1]").val();
+    }else{
+        alert("Password does not match");
+    }
 //    alert("success");
     $.Notify({
         caption: 'Local Storage',
@@ -44,6 +51,16 @@ $("#loginForm").on('submit',function(){
         type: 'success',
         icon:'<img src="icon2.png">'
     });
+});
+
+$("#loginButton").on('click', function(){
+   var email = $('input[name=email]').val(),
+       pass = $('input[name=pass]').val();
+    if(email == localStorage.email && pass == localStorage.pass){
+        window.location.assign('list.html');
+    }else{
+        alert("Wrong credentials!");
+    }
 });
 function showData(){
     var name = "<dt>Name:</dt><dd>"+localStorage.name+"</dd>",
@@ -58,7 +75,12 @@ function clearLocal(){
     $.Notify({
         caption: 'Local Storage',
         content: 'Local Storage data cleared',
-        type: 'success',
+        type: 'alert',
         icon:'<img src="icon2.png">'
     });
-}
+};
+
+function loadList(){
+    var row = "<tr><td>"+localStorage.name+"</td><td>"+localStorage.email+"</td><td>"+localStorage.pass+"</td></tr>";
+    $("table#listTable tbody").html(row);
+};
